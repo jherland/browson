88ac2605df138e0932f5d9e6a1b586037df7899e
Johan Herland <johan@herland.net>
2021-04-07 01:06:43 +0200

    WIP: More improvements, especially re. drawing and speed

--- browson.py
+++ browson.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-from contextlib import suppress
-from functools import partial
+from contextlib import contextmanager, suppress
+from functools import partial, wraps
 import json
 import logging
+import signal
 import sys
+from time import monotonic as now
 
 from blessed import Terminal
 
@@ -13,7 +15,7 @@ from style import PythonStyle, JSONStyle, Colorizer, LineWrapper
 logger = logging.getLogger("browson")
 
 
-def render(node, style):
+def render_nodes(root, style):
     prefixes = [""]
     suffixes = [""]
 
@@ -55,7 +57,7 @@ def render(node, style):
             s = style.close(n)
             yield style.format_line(prefix, s, suffix, n)
 
-    yield from node.dfwalk(previsit, postvisit)
+    yield from root.dfwalk(previsit, postvisit)
 
 
 def clamp(val, minimum, maximum):
@@ -63,34 +65,110 @@ def clamp(val, minimum, maximum):
     return minimum if val < minimum else maximum if val > maximum else val
 
 
+def debug_time(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        try:
+            t = now()
+            return f(*args, **kwargs)
+        finally:
+            logger.debug(f"{f.__name__} took {now() - t}s")
+
+    return wrapper
+
+
+@contextmanager
+def signal_handler(signalnum, handler):
+    """Install the given signal handler for the duration of this context."""
+
+    def wrapped_handler(signum, frame):
+        logger.debug(f"signal handler invoked with {signum}, {frame}")
+        handler()
+
+    prev = signal.signal(signalnum, wrapped_handler)
+    try:
+        yield
+    finally:
+        signal.signal(signalnum, prev)
+
+
+class ResizeEvent(Exception):
+    pass
+
+
 class UI:
-    def __init__(self, term, style):
+    context_lines = 1  # how many context lines around the focused line?
+
+    def __init__(self, root, term, style):
+        self.root = root
         self.term = term
         self.style = style
-        self._quit = False  # set to True when user wants to quit
-        self._status = None  # special status message
-        self._timeout = None  # used to wipe error messages
-        self._focus = 1  # which node/line is currently focused
 
-        self.lines = []
+        # Main UI state variables
+        self.lines = []  # from self.render()
+        self.focus = 1  # which line is currently focused (numbered from 1)
+        self.viewport = (1, 1)  # line span currently visible
+
+        # Misc. state/communication variables
+        self._need_render = True  # must rerender all lines from scratch
+        self._need_draw = True  # must redraw current viewport
+        self._quit = False  # time to quit
+        self._status = None  # custom status line
+        self._timeout = None  # used to reset status line
+
+    # Actions triggered from input
 
     def reset_status(self):
         self._status = None
         self._timeout = None
+        self.draw_status()
+
+    def set_focus(self, line):
+        self.focus = clamp(line, 1, len(self.lines))
+        self.adjust_viewport()
 
     def move_focus(self, relative):
-        self._focus = clamp(self._focus + relative, 1, len(self.lines))
+        return self.set_focus(self.focus + relative)
+
+    def adjust_viewport(self):
+        height = self.term.height - 2
+        start, _ = self.viewport
+
+        if self.focus < start:  # scroll viewport up
+            start = self.focus - self.context_lines
+        elif self.focus > start + height:  # scroll viewport down
+            start = self.focus + self.context_lines - height
+        # Keep viewport within rendered lines
+        start = max(1, min(len(self.lines), start + height) - height)
+
+        self.viewport = (start, start + height)
+        self._need_draw = True
+
+    def on_resize(self):
+        self.adjust_viewport()
+        raise ResizeEvent()  # trigger exit from keyboard polling
+
+    def rerender(self):
+        self._need_render = True
+
+    def redraw(self):
+        self._need_draw = True
 
     def quit(self):
         self._quit = True
 
-    def error(self, message):
-        self._status = self.term.bright_red(message)
+    def warning(self, message):
+        logger.warning(message)
+        self._status = self.term.bright_red(self.term.center(message))
         self._timeout = 3
+        self.draw_status()
+
+    # End of actions
 
     def handle_key(self, keystroke):
         logger.debug(f"Got keystroke: {(str(keystroke), keystroke.name)!r}")
         keymap = {
+            # focus movement
             "k": partial(self.move_focus, -1),
             "j": partial(self.move_focus, 1),
             "KEY_UP": partial(self.move_focus, -1),
@@ -99,53 +177,85 @@ class UI:
             "KEY_SDOWN": partial(self.move_focus, 5),
             "KEY_PGUP": partial(self.move_focus, -(self.term.height - 3)),
             "KEY_PGDOWN": partial(self.move_focus, self.term.height - 3),
-            # TODO:
-            # - help window with keymap
-            # - navigate to matching bracket/parent/sibling
-            # - search
-            # - filter
-            # - expand/collapse
-            # - SPEED (do not rerender everything)!
+            "KEY_HOME": partial(self.set_focus, 1),
+            "KEY_END": partial(self.set_focus, len(self.lines)),
+            # re-render/re-draw
+            "KEY_F5": self.rerender,
+            "\x0c": self.redraw,  # Ctrl+L
+            # quitting
             "q": self.quit,
-            "\x04": self.quit,  # EOF
-            None: partial(self.error, f"Unknown key {keystroke!r}!"),
+            "\x04": self.quit,  # EOF, Ctrl+D
+            None: partial(self.warning, f"Unknown key {keystroke!r}!"),
         }
         for key in [str(keystroke), keystroke.name, None]:
             with suppress(KeyError):
                 return keymap[key]
 
     def status(self):
-        if self._status is not None:
-            return self._status
+        if self._status is None:  # use default
+            return self.term.rjust(
+                f"- ({self.focus}/{len(self.lines)} lines) -"
+            )
         else:
-            return f"--- ({self._focus}/{len(self.lines)} lines)"
+            return self._status
 
-    def run(self, root):
+    def highlight_line(self, line, variant="on_gray20"):
+        return getattr(self.term, variant)(self.term.ljust(line))
+
+    def draw_status(self):
+        with self.term.location(0, self.term.height - 1):
+            print(self.highlight_line(self.status(), "reverse"), end="", flush=True)
+
+    @debug_time
+    def render(self):
+        self.lines = list(render_nodes(self.root, self.style))
+        assert all("\n" not in line for line in self.lines)
+        self.adjust_viewport()
+        self._need_render = False
+
+    @debug_time
+    def draw(self):
+        print(self.term.home + self.term.clear, end="")
+        start, end = self.viewport
+        for n, line in enumerate(self.lines[start - 1 : end], start):
+            if n == self.focus:
+                line = self.highlight_line(line)
+            print(line)
+        self.draw_status()
+        self._need_draw = False
+
+    def dump(self):
+        self.render()
+        print("\n".join(self.lines))
+
+    def run(self):
         term = self.term
-        interactive = term.is_a_tty
-        # if True:
         with term.fullscreen(), term.cbreak(), term.hidden_cursor():
-            while not self._quit:
-                self.lines = list(render(root, self.style))
-                # assert all("\n" not in line for line in lines)
-                print(term.home + term.clear, end="")
-                for n, line in enumerate(self.lines, 1):
-                    if n == self._focus:
-                        line = term.reverse(line)
-                    print(line)
-                if not interactive:
-                    break
-                print(self.status(), end="", flush=True)
-                keystroke = term.inkey(timeout=self._timeout)
-                self.reset_status()
-                if keystroke:
-                    self.handle_key(keystroke)()
+            with signal_handler(signal.SIGWINCH, self.on_resize):
+                while not self._quit:
+                    with suppress(ResizeEvent):
+                        if self._need_render:
+                            self.render()
+                        if self._need_draw:
+                            self.draw()
+                        keystroke = term.inkey(timeout=self._timeout)
+                        self.reset_status()
+                        if keystroke:
+                            self.handle_key(keystroke)()
 
         logger.info("Bye!")
 
 
 class MyStyle(LineWrapper, Colorizer, JSONStyle):
     pass
+
+
+# TODO:
+# - help window with keymap
+# - navigate to matching bracket/parent/sibling
+# - search
+# - filter
+# - expand/collapse
 
 
 def main():
@@ -155,11 +265,15 @@ def main():
     with open(sys.argv[1], "rb") as f:
         data = json.load(f)
     logger.info("Building nodes...")
-    node = Node.build(data)
+    root = Node.build(data)
 
     logger.info("Running app...")
     term = Terminal()
-    UI(term, MyStyle(term=term)).run(node)
+    ui = UI(root, term, MyStyle(term=term))
+    if term.is_a_tty:
+        ui.run()
+    else:
+        ui.dump()
 
 
 if __name__ == "__main__":
--- test_browson.py
+++ test_browson.py
@@ -1,20 +1,25 @@
+import json
+import subprocess
+import sys
+from tempfile import NamedTemporaryFile
 import textwrap
 
-from browson import render
+import browson
+from browson import render_nodes
 from node import Node
 from style import PythonStyle, JSONStyle
 
 
-class Test_render_python:
+class Test_render_nodes_python:
     style = PythonStyle()
 
     def test_leaf_node(self):
         n = Node.build("foo")
-        assert "\n".join(render(n, self.style)) == repr("foo")
+        assert "\n".join(render_nodes(n, self.style)) == repr("foo")
 
     def test_simple_list(self):
         n = Node.build(["foo", 123, True])
-        assert "\n".join(render(n, self.style)) == textwrap.dedent(
+        assert "\n".join(render_nodes(n, self.style)) == textwrap.dedent(
             """\
             [
                 'foo',
@@ -25,7 +30,7 @@ class Test_render_python:
 
     def test_simple_dict(self):
         n = Node.build({"foo": 123, "bar": 456, "baz": 789})
-        assert "\n".join(render(n, self.style)) == textwrap.dedent(
+        assert "\n".join(render_nodes(n, self.style)) == textwrap.dedent(
             """\
             {
                 'foo': 123,
@@ -36,7 +41,7 @@ class Test_render_python:
 
     def test_nested_dict(self):
         n = Node.build({"foo": {"a": 1, "b": 2}, "bar": {"c": 3, "d": 4}})
-        assert "\n".join(render(n, self.style)) == textwrap.dedent(
+        assert "\n".join(render_nodes(n, self.style)) == textwrap.dedent(
             """\
             {
                 'foo': {
@@ -51,16 +56,16 @@ class Test_render_python:
         )
 
 
-class Test_render_json:
+class Test_render_nodes_json:
     style = JSONStyle()
 
     def test_leaf_node(self):
         n = Node.build("foo")
-        assert "\n".join(render(n, self.style)) == '"foo"'
+        assert "\n".join(render_nodes(n, self.style)) == '"foo"'
 
     def test_simple_list(self):
         n = Node.build(["foo", 123, True, False, None])
-        assert "\n".join(render(n, self.style)) == textwrap.dedent(
+        assert "\n".join(render_nodes(n, self.style)) == textwrap.dedent(
             """\
             [
               "foo",
@@ -73,7 +78,7 @@ class Test_render_json:
 
     def test_simple_dict(self):
         n = Node.build({"foo": 123, "bar": 456, "baz": 789})
-        assert "\n".join(render(n, self.style)) == textwrap.dedent(
+        assert "\n".join(render_nodes(n, self.style)) == textwrap.dedent(
             """\
             {
               "foo": 123,
@@ -84,7 +89,7 @@ class Test_render_json:
 
     def test_nested_dict(self):
         n = Node.build({"foo": {"a": 1, "b": 2}, "bar": {"c": 3, "d": 4}})
-        assert "\n".join(render(n, self.style)) == textwrap.dedent(
+        assert "\n".join(render_nodes(n, self.style)) == textwrap.dedent(
             """\
             {
               "foo": {
@@ -96,4 +101,35 @@ class Test_render_json:
                 "d": 4
               }
             }"""
+        )
+
+
+class Test_noninteractive_dump:
+    def run_on_json_data(self, data):
+        with NamedTemporaryFile(mode="w+") as f:
+            json.dump(data, f)
+            f.flush()
+            proc = subprocess.run(
+                [sys.executable, browson.__file__, f.name],
+                stdout=subprocess.PIPE,
+                encoding="UTF-8",
+                check=True,
+            )
+            return proc.stdout
+
+    def test_nested_dict(self):
+        data = {"foo": {"a": 1, "b": 2}, "bar": {"c": 3, "d": 4}}
+        assert self.run_on_json_data(data) == textwrap.dedent(
+            """\
+            {
+              "foo": {
+                "a": 1,
+                "b": 2
+              },
+              "bar": {
+                "c": 3,
+                "d": 4
+              }
+            }
+            """
         )

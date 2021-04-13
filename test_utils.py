4b016c2677f0238fd361e3847ec9c3b132005f34
Johan Herland <johan@herland.net>
2021-04-14 01:27:52 +0200

    Add LazyList to try to delay rendering lines outside the current viewport
    Not yet in use, and its semantics are tricky enough that it might never
    get used...

---
+++ test_utils.py
@@ -0,0 +1,112 @@
+import pytest
+
+from utils import clamp, LazyList
+
+
+def test_clamp():
+    assert clamp(-3, 0, 100) == 0
+    assert clamp(1.23, 0, 100) == 1.23
+    assert clamp(-1.23, 0, 100) == 0
+    assert clamp(-1, -1, 1) == -1
+    assert clamp(1, -1, 1) == 1
+    assert clamp(0, -1, 1) == 0
+    assert clamp(-2, -1, 1) == -1
+    assert clamp(2, -1, 1) == 1
+
+
+class Test_LazyList:
+    def iterator(self, values):
+        for value in values:
+            yield value
+
+    def test_empty_iterator(self):
+        ll = LazyList(self.iterator([]))
+        assert len(ll) == 0
+        with pytest.raises(IndexError):
+            ll[0]
+        assert ll == []
+
+    def test_simple_iterator_access_in_sequence(self):
+        ll = LazyList(self.iterator([6, 5, 4]))
+        assert len(ll) == 0
+        assert ll[0] == 6
+        assert len(ll) == 1
+        assert ll[1] == 5
+        assert len(ll) == 2
+        assert ll[2] == 4
+        assert len(ll) == 3
+        assert ll == [6, 5, 4]
+        with pytest.raises(IndexError):
+            ll[3]
+        assert ll == [6, 5, 4]
+
+    def test_simple_iterator_random_access(self):
+        ll = LazyList(self.iterator([6, 5, 4, 3, 2, 1]))
+        assert len(ll) == 0
+        assert ll[2] == 4
+        assert ll == [6, 5, 4]
+        assert ll[0] == 6
+        assert len(ll) == 3
+        assert ll[5] == 1
+        assert len(ll) == 6
+        assert ll == [6, 5, 4, 3, 2, 1]
+
+    def test_can_append_after_iterator_exhausted(self):
+        ll = LazyList(self.iterator([6, 5, 4]))
+        assert ll[2] == 4
+        ll.append(3)
+        assert ll[3] == 3
+        assert len(ll) == 4
+
+    def test_can_append_before_iterator_exhausted(self):
+        ll = LazyList(self.iterator([6, 5, 4]))
+        assert ll[0] == 6
+        ll.append(3)
+        ll.append(2)
+        ll.append(1)
+        assert ll[3] == 1
+        assert len(ll) == 4
+        assert ll[4] == 5
+        assert ll[5] == 4
+        assert len(ll) == 6
+        with pytest.raises(IndexError):
+            ll[6]
+        assert ll == [6, 3, 2, 1, 5, 4]
+
+    def test_can_get_slices(self):
+        ll = LazyList(self.iterator([6, 5, 4, 3, 2, 1]))
+        assert ll[1:3] == [5, 4]
+        assert ll[:4] == [6, 5, 4, 3]
+        assert ll[4:] == [2, 1]
+        ll = LazyList(self.iterator([6, 5, 4, 3, 2, 1]))
+        assert ll[4:7] == [2, 1]
+
+    def test_can_delete_slices(self):
+        ll = LazyList(self.iterator([6, 5, 4, 3, 2, 1]))
+        assert ll[2] == 4
+        del ll[1:]
+        assert ll[2] == 2
+        del ll[1:2]
+        assert ll[2] == 1
+        assert len(ll) == 3
+        assert ll == [6, 2, 1]
+
+    def test_open_ended_set_slice_empties_iterator(self):
+        ll = LazyList(self.iterator([6, 5, 4, 3, 2, 1]))
+        assert ll[2] == 4
+        ll[1:] = [7, 8, 9]  # wipes generator
+        assert ll == [6, 7, 8, 9]
+        with pytest.raises(IndexError):
+            ll[4]
+
+    def test_can_insert_slices(self):
+        ll = LazyList(self.iterator([6, 5, 4, 3, 2, 1]))
+        assert ll[2] == 4
+        ll[1:3] = [7, 8, 9]
+        assert ll == [6, 7, 8, 9]
+        assert ll[4] == 3
+        ll[5:6] = [10, 11, 12]  # extract int(2) and immediately replace it
+        assert ll == [6, 7, 8, 9, 3, 10, 11, 12]
+        with pytest.raises(IndexError):
+            ll[10]  # extracts int(1) to index 8, but fails to extract any more
+        assert ll == [6, 7, 8, 9, 3, 10, 11, 12, 1]
--- utils.py
+++ utils.py
@@ -1,11 +1,16 @@
 from collections.abc import MutableSequence
 from contextlib import contextmanager
 from functools import wraps
+from itertools import islice
 import logging
 import signal
+import sys
 from time import monotonic as now
+from typing import Iterator, List, Optional, TypeVar
 
 logger = logging.getLogger(__name__)
+
+Item = TypeVar("Item")
 
 
 def clamp(val, minimum, maximum):
@@ -14,6 +19,8 @@ def clamp(val, minimum, maximum):
 
 
 def debug_time(f):
+    """Decorator to produce debug log messages with function run times."""
+
     @wraps(f)
     def wrapper(*args, **kwargs):
         try:
@@ -38,3 +45,67 @@ def signal_handler(signalnum, handler):
         yield
     finally:
         signal.signal(signalnum, prev)
+
+
+class LazyList(MutableSequence):
+    """A list interface to an iterator, that extract elements on-demand.
+
+    The behavior of this list is probably not completely well-defined: Some
+    operations behave as if this is a "regular" list of the elements that have
+    been extracted so far (e.g. len(ll) or negative indexing: ll[-1]), whereas
+    others will cause the list to "magically" grow by extracting elements from
+    the iterator
+    - Accessing elements beyond of the end of .realized (i.e. ll[x] where
+      x >= len(ll))
+    - TODO: ???
+    """
+
+    def __init__(self, iterator: Optional[Iterator[Item]] = None):
+        self.iterator: Optional[Iterator[Item]] = iterator
+        self.realized: List[Item] = []
+
+    def __repr__(self) -> str:
+        return f"{self.realized}/{self.iterator}"
+
+    def __eq__(self, other: "LazyList") -> bool:
+        return self.realized == other
+
+    def __len__(self) -> int:
+        return len(self.realized)  # this is a lie
+
+    def _extract_until_len(self, n) -> None:
+        if n > len(self.realized) and self.iterator is not None:
+            self.realized.extend(islice(self.iterator, n - len(self.realized)))
+
+    @debug_time
+    def real_length(self):
+        self._extract_until_len(sys.maxsize)
+        return len(self.realized)
+
+    def __getitem__(self, i):
+        if isinstance(i, slice):
+            # Extract until end-of-slice
+            end = sys.maxsize if i.stop is None else i.stop
+        else:
+            end = i + 1
+        self._extract_until_len(end)
+        return self.realized.__getitem__(i)
+
+    def __delitem__(self, i):
+        return self.realized.__delitem__(i)
+
+    def __setitem__(self, i, value):
+        # We're replacing an item (or a slice of items). We must extract items
+        # up to and including the one(s) that are replaced.
+        if isinstance(i, slice):
+            if i.stop is None:  # Exhaust the entire generator
+                # TODO: Is this safe, or must we consider side effects???
+                self.iterator = None
+            else:
+                self._extract_until_len(i.stop)
+        else:
+            self._extract_until_len(i + 1)
+        return self.realized.__setitem__(i, value)
+
+    def insert(self, i, value):
+        return self.realized.insert(i, value)
